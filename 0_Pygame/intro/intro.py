cc1ffa717a20409c560b3233f3492346a5fa1e03
mareis <mads.reistadbakk@mrfylke.no>
2022-11-10 10:02:20 +0100

    ,

--- 0_Pygame/intro/intro.py
+++ 0_Pygame/intro/intro.py
@@ -1,19 +1,44 @@
 from turtle import st
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = pygame.time.get_ticks() - start_time
     score_surf = test_font.render(f'{current_time//1000:10}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
+
+    return current_time//1000 
  
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for rect in obstacle_list:
+            rect.x -= 5 
+
+            screen.blit(snail_surf, rect)
+
+        if obstacle_list[0].x < -  100:
+            obstacle_list.pop(0)
+            #print(obstacle_list[0].x)
+
+             
+    
+            
+            
+    return obstacle_list
+             
+
+
 
 size = width, height = (800, 400)
 fps = 60 
 
 player_v = 0
 g = 0.3
+
+score = 0 
 
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -30,15 +55,33 @@ ground_surf = pygame.image.load('img/ground.png').convert()
 #score_surf = test_font.render('My game', False, (64,64,64)) 
 #score_rect = score_surf.get_rect(center = (width/2, 50))
 
-snail_surf = pygame.image.load('img/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(midbottom = (800, 300))
- 
+
+# Obsticales
+snail_surf = pygame.image.load('img/snail/snail1.png').convert_alpha() 
+fly_surf = pygame.image.load('img/fly/fly1.png').convert_alpha()
+
+obstacle_rect_list = []
+
+
 player_surf = pygame.image.load('img/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300)) 
 
+#
+
 player_stand = pygame.image.load('img/player/player_stand.png')
-player_stand = pygame.transform.scale(player_stand, (200, 400))
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center = (width/2, height/2))
+
+game_name_surf = test_font.render('Snail Run', False, (111,196,169)) 
+game_name_surf = pygame.transform.rotozoom(game_name_surf, 0, 2)
+game_name_rect = game_name_surf.get_rect(center = (width/2, 70))
+
+game_message_surf = test_font.render('Press space to run', False, (111,196,169)) 
+game_message_rect = game_message_surf.get_rect(center = (width/2, height-70))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500) 
 
 while True:
     for event in pygame.event.get():
@@ -51,33 +94,46 @@ while True:
                 if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                     player_v = -8
 
+            if event.type == obstacle_timer:
+                if randint(0,2):obstacle_rect_list.append(snail_surf.get_rect(midbottom = (randint(900, 1100), 300)))
+                else: obstacle_rect_list.append(fly_surf.get_rect(midbottom = (randint(900, 1100), 210)))
+
+
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800
                 player_rect.bottom = 300
                 start_time = pygame.time.get_ticks() 
+
+            
   
     if game_active:
         screen.blit(sky_surf, (0, 0)) 
         screen.blit(ground_surf, (0, 300))  
         
-        display_score()
+        score = display_score()
 
         player_rect.y += player_v
         player_v += g
 
         if player_rect.bottom > 300: player_rect.bottom = 300
 
-        
-        screen.blit(snail_surf, snail_rect)
         screen.blit(player_surf, player_rect)
 
-        snail_rect.x -= 4 
-        if snail_rect.right <= 0: snail_rect.left = 800
+        
+        # Obstacle movement 
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+         
 
-        if player_rect.colliderect(snail_rect):
-            game_active = False
+        #screen.blit(snail_surf, snail_rect)
+        #snail_rect.x -= 4 
+
+
+
+        #if snail_rect.right <= 0: snail_rect.left = 800
+
+        #if player_rect.colliderect(snail_rect):
+            #game_active = False
 
         mouse_pos = pygame.mouse.get_pos()
         if player_rect.collidepoint(mouse_pos):
@@ -86,6 +142,15 @@ while True:
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
+
+        game_score_surf = test_font.render(f'Youre score: {score}', False, (111,196,169)) 
+        game_score_rect = game_score_surf.get_rect(center = (width/2, height-70))
+
+        screen.blit(game_name_surf, game_name_rect)
+
+        if score == 0: screen.blit(game_message_surf, game_message_rect)
+        else: screen.blit(game_score_surf, game_score_rect)
+
          
     
     pygame.display .update()
